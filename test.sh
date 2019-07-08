daa015fa0d1eed1224a9d9714c6bd4ecc2b9f434
Tony Clarke <tonyc@amdocs.com>
2019-07-07 20:36:05 -0400

    Include results from macbookpro laptop

--- test.sh
+++ test.sh
@@ -30,6 +30,7 @@ cd apigw && mvn spring-boot:run -Dspring-boot.run.profiles=withsleuth &
 sleep 20
 cd gatlingtest && mvn gatling:test -Dtitle=withsleuth -Dnusers=750
 curl -X POST http://localhost:8080/actuator/shutdown
+curl -X POST http://localhost:9090/actuator/shutdown 
 cd ..
 
 cd restapp && mvn spring-boot:run &
